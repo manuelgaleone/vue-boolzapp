9e121b327d98e0b52ff1284db75b3cd53f6e1f34
manuelgaleone <galeonemanuel14@gmail.com>
2022-11-03 14:20:07 +0100

    Working on the add button

--- Milestone 3/assets/js/main.js
+++ Milestone 3/assets/js/main.js
@@ -1,18 +1,18 @@
 /*
 Replica della grafica con la possibilità di avere messaggi scritti dall’utente (verdi) e dall’interlocutore (bianco) assegnando due classi CSS diverse.
 
-Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto
+Visualizzazione dinamica della lista contatti: tramite la direttiva v-for, visualizzare nome e immagine di ogni contatto.
 */
 
 /*
 Milestone 2
-    ● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
+    ● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione.
 
-    ● Click sul contatto mostra la conversazione del contatto cliccato
+    ● Click sul contatto mostra la conversazione del contatto cliccato.
 
 Milestone 3
     ● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
-    “enter” il testo viene aggiunto al thread sopra, come messaggio verde
+    “enter” il testo viene aggiunto al thread sopra, come messaggio verde.
 
     ● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
     un “ok” come risposta, che apparirà dopo 1 secondo.
@@ -20,10 +20,10 @@ Milestone 3
 
 /*
 Milestone 4
-    ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+    ● Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina).
 
-    Milestone 5 - opzionale
-    ● Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
+Milestone 5 - opzionale
+    ● Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato.
 */
 
 const { createApp } = Vue;
@@ -195,6 +195,7 @@ const { createApp } = Vue;
                 ],
             }
         ],
+        messages: "",
         activeIndex: 0,
       }
     },
@@ -204,6 +205,9 @@ const { createApp } = Vue;
             this.activeIndex = i;
         
         },
+        addMessage() {
+
+        }
     },
   }).mount('#app')
 
